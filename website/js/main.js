f7296841cd32ca8d3564996a2a46cb4cf1c73b4e
Brian Maher <briancmaher@gmail.com>
2016-03-12 13:20:56 +0100

    Add directions for meeting rooms between elevators.

--- website/js/main.js
+++ website/js/main.js
@@ -5,20 +5,23 @@ var json = {
       "name": "marlene dietrich",
       "building": 1,
       "floor": 4,
-      "elevator": "east or west",
-      "direction": "right or left",
+      "cardinal": "north",
+      "elevator": "either",
+      "direction": "between",
     }, {
       "number": "1.41C-025",
       "name": "harald juhnke",
       "building": 1,
       "floor": 4,
-      "elevator": "east or west",
-      "direction": "right or left"
+      "cardinal": "north",
+      "elevator": "either",
+      "direction": "between"
     }, {
       "number": "1.41C-078",
       "name": "billy wilder",
       "building": 1,
       "floor": 4,
+      "cardinal": "north east",
       "elevator": "east",
       "direction": "left"
     }, {
@@ -26,6 +29,7 @@ var json = {
       "name": "belvedere",
       "building": 1,
       "floor": 4,
+      "cardinal": "east",
       "elevator": "east",
       "direction": "right"
     }, {
@@ -33,6 +37,7 @@ var json = {
       "name": "charlottenburg",
       "building": 1,
       "floor": 4,
+      "cardinal": "south east",
       "elevator": "east",
       "direction": "right"
     }, {
@@ -40,31 +45,18 @@ var json = {
       "name": "hasenheide",
       "building": 1,
       "floor": 4,
+      "cardinal": "west",
       "elevator": "west",
       "direction": "left"
     }]
   }
 };
 
-function buildString(v) {
-  return `${titleCase(v.name)} is on the ${getOrdinal(v.floor)} floor in HERE building `
-    + `${v.building}. From the ${v.elevator} elevator go ${v.direction}.`;
-}
-
-function titleCase(s) {
-  return s.charAt(0).toUpperCase() + s.slice(1);
-}
-
-function getOrdinal(n) {
-  var s = ['th', 'st', 'nd', 'rd'], v = n % 100;
-  return n + (s[(v - 20) % 10] || s[v] || s[0]);
-}
-
 $('form').submit(function(event) {
   var found = false;
   $.each(json.rooms.room, function(i, v) {
     if (v.name.toLowerCase() == $('input').val().toLowerCase()) {
-      $('span').text(buildString(v)).show();
+      $('span').text(getDirections(v)).show();
       found = true;
       return;
     }
@@ -74,3 +66,38 @@ $('form').submit(function(event) {
   }
   event.preventDefault();
 });
+
+function getDirections(v) {
+  return `${toTitleCase(v.name)} is on the ${getOrdinal(v.floor)} floor in HERE building `
+    + `${v.building}, ${getCardinal(v.cardinal)}. ${getElevator(v.elevator, v.direction)}.`;
+}
+
+function toTitleCase(s)
+{
+  return s.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+}
+
+function getOrdinal(n) {
+  var s = ['th', 'st', 'nd', 'rd'], v = n % 100;
+  return n + (s[(v - 20) % 10] || s[v] || s[0]);
+}
+
+function getCardinal(c) {
+  if (wordCount(c) > 1) {
+    return `${c} corner`
+  } else {
+    return c
+  }
+}
+
+function wordCount(s) { 
+  return s.split(' ').length;
+}
+
+function getElevator(e, d) {
+  if (e == 'either') {
+    return `Take ${e} elevator, the room is ${d} them`
+  } else {
+    return `From the ${e} elevator, go ${d}`
+  }
+}
